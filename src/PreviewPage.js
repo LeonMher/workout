a201b82a063c1e9686f5e1fb636b39ad6b9a1913
Mher <99.p3rc3n7@gmail.com>
2022-04-01 15:09:50 +0400

    almost responsive

--- src/PreviewPage.js
+++ src/PreviewPage.js
@@ -4,7 +4,9 @@ import React from 'react';
 function PreviewPage(){
     return(
         <div className='previewPage'>
-            <div className='firstCol'></div>
+            <div className='firstCol'>
+                <img className='img' src={'https://images.unsplash.com/photo-1590487988256-9ed24133863e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=728&q=80'} />
+            </div>
             <div className='secondCol'>
                 <h1>About <span>A.M.F.</span></h1>
                 <p>
