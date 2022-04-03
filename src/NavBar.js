5102875a1ab7a16269cba97d9a157132c2b94435
Mher <99.p3rc3n7@gmail.com>
2022-04-03 18:39:56 +0400

    all responsives fixed

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 function NavBar(){
 
-    const [toggle, setToggle] = useState(true)
+    const [toggle, setToggle] = useState(false)
 
     const toggleNav = ()=>{
         setToggle(!toggle)
