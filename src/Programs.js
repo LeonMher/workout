fb9d5a9108f5ca494c0b215fc71a5ba8209f7946
Mher <99.p3rc3n7@gmail.com>
2022-04-04 15:30:10 +0400

    workout plan fully added

--- src/Programs.js
+++ src/Programs.js
@@ -52,7 +52,7 @@ function Programs(){
                         setName(e.target.value)
                     })}/>
 
-                    <input type='text' name="theDay" placeholder="Day of week" onChange={((e)=>{
+                    <input type='text'  name="theDay" placeholder="Day of week" onChange={((e)=>{
                         setDay(e.target.value)
                     })}/>
                     <input type='number' name="theAge" placeholder="Age" onChange={((e)=>{
@@ -69,6 +69,8 @@ function Programs(){
                 <>
                      <h1>Individual workout plan for {name}</h1>
                         <div className="individualPlan">
+                        <h2 >Monday</h2>
+                        <h4>Chest/Biceps</h4>
                         <p>1. Do some {workoutPlan[0].firstExercise[0].nameOfExercise}, {workoutPlan[0].firstExercise[0].reps} times, {workoutPlan[0].firstExercise[0].sets} sets  </p>
                         <p>2. Then do {workoutPlan[0].secondExercise[0].nameOfExercise}, {workoutPlan[0].secondExercise[0].reps} times, {workoutPlan[0].firstExercise[0].sets} sets  </p>
                         <p>3. Do some {workoutPlan[0].thirdExercise[0].nameOfExercise}, {workoutPlan[0].thirdExercise[0].reps} times, {workoutPlan[0].firstExercise[0].sets} sets  </p>
@@ -81,6 +83,66 @@ function Programs(){
                 
                 
                 : <p>Your personal workout plan will be displayed here.</p>}
+
+
+                    
+                    {age >= 21 && day === "Wednesday" ? 
+                <>
+                     <h1>Individual workout plan for {name}</h1>
+                        <div className="individualPlan">
+                        <h2 >Wednesday</h2>
+                        <h4 >Shoulders/Triceps</h4>
+                        <p>1. Do some {workoutPlan[1].firstExercise[0].nameOfExercise}, {workoutPlan[1].firstExercise[0].reps} times, {workoutPlan[1].firstExercise[0].sets} sets  </p>
+                        <p>2. Then do {workoutPlan[1].secondExercise[0].nameOfExercise}, {workoutPlan[1].secondExercise[0].reps} times, {workoutPlan[1].firstExercise[0].sets} sets  </p>
+                        <p>3. Do some {workoutPlan[1].thirdExercise[0].nameOfExercise}, {workoutPlan[1].thirdExercise[0].reps} times, {workoutPlan[1].firstExercise[0].sets} sets  </p>
+                        <p>4. Do some {workoutPlan[1].forthExercise[0].nameOfExercise}, {workoutPlan[1].forthExercise[0].reps} times, {workoutPlan[1].firstExercise[0].sets} sets  </p>
+                        <p>5. Do some {workoutPlan[1].fifthExercise[0].nameOfExercise}, {workoutPlan[1].fifthExercise[0].reps} times, {workoutPlan[1].firstExercise[0].sets} sets  </p>
+                        </div>
+                </>
+              
+                
+                
+                
+                : null}
+
+                    {age >= 21 && day === "Friday" ? 
+                <>
+                     <h1>Individual workout plan for {name}</h1>
+                        <div className="individualPlan">
+                        <h2 >Friday</h2>
+                        <h4 >Legs only!</h4>
+                        <p>1. Do some {workoutPlan[2].firstExercise[0].nameOfExercise}, {workoutPlan[2].firstExercise[0].reps} times, {workoutPlan[2].firstExercise[0].sets} sets  </p>
+                        <p>2. Then do {workoutPlan[2].secondExercise[0].nameOfExercise}, {workoutPlan[2].secondExercise[0].reps} times, {workoutPlan[2].firstExercise[0].sets} sets  </p>
+                        <p>3. Do some {workoutPlan[2].thirdExercise[0].nameOfExercise}, {workoutPlan[2].thirdExercise[0].reps} times, {workoutPlan[2].firstExercise[0].sets} sets  </p>
+                        <p>4. Do some {workoutPlan[2].forthExercise[0].nameOfExercise}, {workoutPlan[2].forthExercise[0].reps} times, {workoutPlan[2].firstExercise[0].sets} sets  </p>
+                        <p>5. Do some {workoutPlan[2].fifthExercise[0].nameOfExercise}, {workoutPlan[2].fifthExercise[0].reps} times, {workoutPlan[2].firstExercise[0].sets} sets  </p>
+                        </div>
+                </>
+              
+                
+                
+                
+                : null}
+                
+
+
+
+                    {age >= 21 && day === "Saturday" ? 
+                <>
+                     <h1>Individual workout plan for {name}</h1>
+                        <div className="individualPlan">
+                        <p>1. Do some {workoutPlan[3].firstExercise[0].nameOfExercise}, {workoutPlan[3].firstExercise[0].reps} times, {workoutPlan[3].firstExercise[0].sets} sets  </p>
+                        <p>2. Then do {workoutPlan[3].secondExercise[0].nameOfExercise}, {workoutPlan[3].secondExercise[0].reps} times, {workoutPlan[3].secondExercise[0].sets} sets  </p>
+                        <p>3. Do some {workoutPlan[3].thirdExercise[0].nameOfExercise}, {workoutPlan[3].thirdExercise[0].reps} times, {workoutPlan[3].thirdExercise[0].sets} sets  </p>
+                        <p>4. Do some {workoutPlan[3].forthExercise[0].nameOfExercise}, {workoutPlan[3].forthExercise[0].reps} times, {workoutPlan[3].forthExercise[0].sets} sets  </p>
+                     
+                        </div>
+                </>
+              
+                
+                
+                
+                : null}
             </div>
             
         </div>
--- src/WorkoutPlan.js
+++ src/WorkoutPlan.js
@@ -6,7 +6,36 @@ const workoutPlan = [
         thirdExercise: [{nameOfExercise: "Machine chest press", reps: 12, sets: 3}],
         forthExercise: [{nameOfExercise: "Barbell Curl", reps: 8, sets: 5}],
         fifthExercise: [{nameOfExercise: "Cable Curl", reps: 10, sets: 3}],
+    },
+
+    {
+        dayOfWeek:"Tuesday",
+        firstExercise: [{nameOfExercise: "Overhead Press", reps: 10, sets: 5}],
+        secondExercise: [{nameOfExercise: "Lateral Raise", reps: 10, sets: 5}],
+        thirdExercise: [{nameOfExercise: "Cable Face Pulls", reps: 12, sets: 3}],
+        forthExercise: [{nameOfExercise: "Dips", reps: 20, sets: 3}],
+        fifthExercise: [{nameOfExercise: "Rope Pushdowns", reps: 10, sets: 5}],
+    },
+
+    {
+        dayOfWeek:"Wednesday",
+        firstExercise: [{nameOfExercise: "Lat Pulldown", reps: 10, sets: 5}],
+        secondExercise: [{nameOfExercise: "Dumbbell Single-Arm Row", reps: 10, sets: 5}],
+        thirdExercise: [{nameOfExercise: "Bent-Over Barbell Rows", reps: 8, sets: 3}],
+        forthExercise: [{nameOfExercise: "Seated Cable Row with Pause", reps: 12, sets: 5}],
+        fifthExercise: [{nameOfExercise: "Shrugs", reps: 10, sets: 5}],
+    },
+
+    {
+        dayOfWeek:"Thursday",
+        firstExercise: [{nameOfExercise: "Squats", reps: 10, sets: 5}],
+        secondExercise: [{nameOfExercise: "Leg Extension", reps: 10, sets: 5}],
+        thirdExercise: [{nameOfExercise: "Sumo Squat", reps: 8, sets: 3}],
+        forthExercise: [{nameOfExercise: "Seated leg press", reps: 12, sets: 5}],
+        
     }
+
+
 ]
 
 
